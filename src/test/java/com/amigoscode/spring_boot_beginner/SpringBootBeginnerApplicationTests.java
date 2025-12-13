df68a881051a4b5a827098dbfcd7042362770d02
Javier de Tena <javidetena97@gmail.com>
2025-12-13 10:34:43 +0100

    First commit

---
+++ src/test/java/com/amigoscode/spring_boot_beginner/SpringBootBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigoscode.spring_boot_beginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
